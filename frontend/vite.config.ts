8f8e63d85e309cea23cc08f54e74d42d6b25c7cc
surfsurfmasurf <surfsurfmasurf@gmail.com>
2026-02-11 00:38:08 +0900

    fix: Add base URL '/h2/' to vite.config.ts for Nginx subpath

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/h2/', // Needed for subpath deployment
   plugins: [react()],
   server: {
     proxy: {
